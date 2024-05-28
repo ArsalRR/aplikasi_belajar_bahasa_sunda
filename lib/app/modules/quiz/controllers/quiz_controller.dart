240c4adb793bbd2b685b848297d74f056b185a88
My mac <mymac@Mys-MacBook-Pro.local>
2024-05-28 19:06:15 +0700

    tambah fitur quiz

--- lib/app/modules/quiz/controllers/quiz_controller.dart
+++ lib/app/modules/quiz/controllers/quiz_controller.dart
@@ -1,23 +1,76 @@
 import 'package:get/get.dart';
 
 class QuizController extends GetxController {
-  //TODO: Implement QuizController
+ final questions = const [
+  {
+    'questionText': '1. Naon basa Sundana "makan"?',
+    'answers': [
+      {'text': 'Nulis', 'score': -2},
+      {'text': 'Maca', 'score': -2},
+      {'text': 'Dahar', 'score': 20},
+      {'text': 'Diajar', 'score': -2},
+    ],
+  },
+  {
+    'questionText': '2. Naon basa Sundana "minum"?',
+    'answers': [
+      {'text': 'Tuang', 'score': -2},
+      {'text': 'Hees', 'score': -2},
+      {'text': 'Maén', 'score': -2},
+      {'text': 'Nginum', 'score': 20},
+    ],
+  },
+  {
+    'questionText': '3. Naon basa Sundana "tidur"?',
+    'answers': [
+      {'text': 'Nyarios', 'score': -2},
+      {'text': 'Sare', 'score': 20},
+      {'text': 'Diajar', 'score': -2},
+      {'text': 'Sumping', 'score': -2},
+    ],
+  },
+  {
+    'questionText': '4. Naon basa Sundana "berbicara"?',
+    'answers': [
+      {'text': 'Nyarios', 'score': 20},
+      {'text': 'Dahar', 'score': -2},
+      {'text': 'Nginum', 'score': -2},
+      {'text': 'Maén', 'score': -2},
+    ],
+  },
+  {
+    'questionText': '5. Naon basa Sundana "bermain"?',
+    'answers': [
+      {'text': 'Nulis', 'score': -2},
+      {'text': 'Maca', 'score': -2},
+      {'text': 'Maén', 'score': 20},
+      {'text': 'Diajar', 'score': -2},
+    ],
+  },
+];
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  var questionIndex = 0.obs;
+  var totalScore = 0.obs;
+  var isLoading = false.obs;
+
+  void answerQuestion(int score) {
+    totalScore.value += score;
+
+    if (questionIndex.value < questions.length - 1) {
+      questionIndex.value++;
+    } else {
+      questionIndex.value = questions.length;  
+    }
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void resetQuiz() {
+    questionIndex.value = 0;
+    totalScore.value = 0;
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void fetchQuizData() async {
+    isLoading.value = true;
+    await Future.delayed(const Duration(seconds: 3));
+    isLoading.value = false;
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/quiz/views/quiz_view.dart
+++ lib/app/modules/quiz/views/quiz_view.dart
@@ -1,23 +1,223 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart'; 
 import '../controllers/quiz_controller.dart';
 
 class QuizView extends GetView<QuizController> {
   const QuizView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('QuizView'),
-        centerTitle: true,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: ClipPath(
+              clipper: WaveClipperOne(),
+              child: Container(
+                height: 150,
+                color: const Color(0xff008DDA),
+                child: AppBar(
+                  title: const Text(
+                    'Quiz Bahasa Sunda',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  centerTitle: true,
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  leading: IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.white),
+                    onPressed: () {
+                      Get.back();
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 100.0),
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Obx(() {
+                if (controller.isLoading.value) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (controller.questionIndex.value < controller.questions.length) {
+                  return Quiz(
+                    answerQuestion: controller.answerQuestion,
+                    questionIndex: controller.questionIndex.value,
+                    questions: controller.questions,
+                  );
+                } else {
+                  return Result(
+                    controller.totalScore.value,
+                    controller.resetQuiz,
+                  );
+                }
+              }),
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'QuizView is working',
-          style: TextStyle(fontSize: 20),
+    );
+  }
+}
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
+
+  const Quiz({
+    Key? key,
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          questions[questionIndex]['questionText'].toString(),
         ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(
+            () => answerQuestion(answer['score'] as int),
+            answer['text'].toString(),
+          );
+        }).toList()
+      ],
+    );
+  }
+}
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  const Question(this.questionText, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: const TextStyle(fontSize: 20),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  const Answer(this.selectHandler, this.answerText, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: SizedBox(
+        height: 50,
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: selectHandler,
+          style: ElevatedButton.styleFrom(
+            primary: const Color(0xff008DDA),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          child: Text(
+            answerText,
+            style: const TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback resetHandler;
+
+  const Result(this.resultScore, this.resetHandler, {Key? key})
+      : super(key: key);
+  String get resultPhrase {
+    String resultText;
+    if (resultScore >= 90) {
+      resultText = 'Sempurna';
+      print(resultScore);
+    } else if (resultScore >= 75) {
+      resultText = 'Lumayan';
+      print(resultScore);
+    } else if (resultScore >= 60) {
+      resultText = 'Tingkatkan Belajarmu';
+    } else if (resultScore >= 10) {
+      resultText = 'Harus Banyak Membaca';
+    } else {
+      resultText = 'Nilai Mu Jelek';
+      print(resultScore);
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            'Score $resultScore',
+            style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 30.0),
+            child: SizedBox(
+              height: 50,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: resetHandler,
+                style: ElevatedButton.styleFrom(
+                  primary: const Color(0xff008DDA),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: const Text(
+                  'Ulangi',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
