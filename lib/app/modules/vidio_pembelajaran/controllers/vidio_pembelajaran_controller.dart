cc5a44eec629cc042adfddc83f76b3ff9d11789c
My mac <mymac@Mys-MacBook-Pro.local>
2024-06-11 05:11:49 +0700

    pp

--- lib/app/modules/vidio_pembelajaran/controllers/vidio_pembelajaran_controller.dart
+++ lib/app/modules/vidio_pembelajaran/controllers/vidio_pembelajaran_controller.dart
@@ -4,10 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class VidioPembelajaranController extends GetxController {
    FirebaseFirestore firestore = FirebaseFirestore.instance;
-  //TODO: Implement VidioPembelajaranController
-  
-
-
   Stream<QuerySnapshot<Object?>> streamData() {
     CollectionReference data = firestore.collection('materi_vidio');
     return data.orderBy('judul', descending: true).snapshots();
