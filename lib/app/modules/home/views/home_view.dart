d84c93d2256182ff0c90a62a7a0fd841523c2d5b
My mac <mymac@Mys-MacBook-Pro.local>
2024-06-16 18:16:53 +0700

    memperbaiki tampilan home dengan flutter carossel untuk memunculkan vidio baru

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:get/get.dart';
 import 'package:quickalert/quickalert.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -36,14 +37,14 @@ class HomeView extends GetView<HomeController> {
                         children: [
                           Obx(() => Text(
                                 'Hai, ${controller.fullName.value}',
-                                style: TextStyle(
+                                style: const TextStyle(
                                   color: Colors.white,
                                   fontSize: 13,
                                   fontWeight: FontWeight.bold,
                                 ),
                               )),
-                          SizedBox(height: 8),
-                          Text(
+                          const SizedBox(height: 8),
+                          const Text(
                             'Selamat Datang Kembali',
                             style: TextStyle(
                               color: Colors.white,
@@ -68,7 +69,7 @@ class HomeView extends GetView<HomeController> {
                                     context: context,
                                     position: RelativeRect.fromLTRB(
                                         size.width, kToolbarHeight + 40, 0, 0),
-                                    items: [
+                                    items: const [
                                       PopupMenuItem<String>(
                                         value: 'setting',
                                         child: Text('Setting'),
@@ -108,14 +109,14 @@ class HomeView extends GetView<HomeController> {
                                           .profileImageUrl.value.isNotEmpty
                                       ? NetworkImage(
                                           controller.profileImageUrl.value)
-                                      : AssetImage('assets/img/gamer.png')
+                                      : const AssetImage('assets/img/gamer.png')
                                           as ImageProvider,
                                   radius: 20,
                                 ),
                               ),
                             );
                           }),
-                          SizedBox(height: 10),
+                          const SizedBox(height: 10),
                         ],
                       ),
                     ),
@@ -134,7 +135,7 @@ class HomeView extends GetView<HomeController> {
                       color: Colors.grey.withOpacity(0.2),
                       spreadRadius: 5,
                       blurRadius: 7,
-                      offset: Offset(0, 3),
+                      offset: const Offset(0, 3),
                     ),
                   ],
                 ),
@@ -145,7 +146,7 @@ class HomeView extends GetView<HomeController> {
                   mainAxisSpacing: 10,
                   shrinkWrap: true,
                   primary: false,
-                  children: [
+                  children: const [
                     MenuWidget(
                       icon_path: "assets/svg/book.svg",
                       title: "Materi Pelajaran",
@@ -164,21 +165,32 @@ class HomeView extends GetView<HomeController> {
                   ],
                 ),
               ),
+           
+              const SizedBox(height: 20),
               Container(
-                transform: Matrix4.translationValues(0.0, -60.0, 0.0),
-                child: Text("Berbasu SMP Serang Baru"),
+                transform: Matrix4.translationValues(0.0, -80.0, 0.0),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  "List Materi Video Baru",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 0.0),
+              Container(
+                transform: Matrix4.translationValues(0.0, -80.0, 0.0),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: StreamBuilder<QuerySnapshot>(
                   stream: controller.streamData(),
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
+                      return const Center(child: CircularProgressIndicator());
                     }
 
                     if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                      return Center(
+                      return const Center(
                         child: Text(
                           'Tidak ada video pembelajaran yang tersedia.',
                           style: TextStyle(fontSize: 18),
@@ -186,11 +198,18 @@ class HomeView extends GetView<HomeController> {
                       );
                     }
 
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
+                    return CarouselSlider.builder(
+                      options: CarouselOptions(
+                        height: 200.0,
+                        autoPlay: true,
+                        enlargeCenterPage: true,
+                        aspectRatio: 2.0,
+                        onPageChanged: (index, reason) {
+                         
+                        },
+                      ),
                       itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
+                      itemBuilder: (context, index, realIndex) {
                         var doc = snapshot.data!.docs[index];
                         var data = doc.data() as Map<String, dynamic>;
                         bool isBlueCard = index % 2 == 0;
@@ -220,7 +239,7 @@ class HomeView extends GetView<HomeController> {
                             }
                           },
                           child: Card(
-                            margin: EdgeInsets.symmetric(
+                            margin: const EdgeInsets.symmetric(
                                 vertical: 8.0, horizontal: 16.0),
                             elevation: 3.0,
                             shape: RoundedRectangleBorder(
@@ -229,21 +248,22 @@ class HomeView extends GetView<HomeController> {
                             child: Container(
                               decoration: BoxDecoration(
                                 color: isBlueCard
-                                    ? Color(0xff008DDA)
+                                    ? const Color(0xff008DDA)
                                     : Colors.white,
                                 borderRadius: BorderRadius.circular(15.0),
                                 boxShadow: [
                                   BoxShadow(
                                     color: Colors.black12,
                                     blurRadius: 4.0,
-                                    offset: Offset(0, 4),
+                                    offset: const Offset(0, 4),
                                   ),
                                 ],
                               ),
                               child: Padding(
-                                padding: EdgeInsets.all(16.0),
+                                padding: const EdgeInsets.all(16.0),
                                 child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
                                     Text(
                                       data['judul'] ?? 'No Title',
@@ -255,19 +275,32 @@ class HomeView extends GetView<HomeController> {
                                             : Colors.black,
                                         fontFamily: 'Poppins',
                                       ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
                                     ),
-                                    SizedBox(height: 10),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Icon(
-                                          Icons.play_circle_fill,
-                                          color: isBlueCard
-                                              ? Colors.white
-                                              : Color(0xff008DDA),
-                                          size: 40,
-                                        ),
-                                      ],
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      data['desc'] ?? 'No Description',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: isBlueCard
+                                            ? Colors.white
+                                            : Colors.black,
+                                        fontFamily: 'Poppins',
+                                      ),
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Icon(
+                                        Icons.play_circle_fill,
+                                        color: isBlueCard
+                                            ? Colors.white
+                                            : const Color(0xff008DDA),
+                                        size: 40,
+                                      ),
                                     ),
                                   ],
                                 ),
