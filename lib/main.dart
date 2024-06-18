b03c7b5a6b85f52926982dde3e2ad75da50f5eff
ferikw <ferikusuma41@gmail.com>
2024-06-18 18:24:04 +0700

    Perbaikan Backend halaman main untuk multiuser

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:capstone_project/app/modules/login/controllers/login_controller.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -13,12 +14,14 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyATHaKgPOBKm7puAv132W6BxPj7Dy5D-Rs",
-          appId: "1:790355774790:android:a0d7d3026b1e4097956947",
-          messagingSenderId: "790355774790",
-          projectId: "my-project-f34c5",
-          storageBucket: "my-project-f34c5.appspot.com"));
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyATHaKgPOBKm7puAv132W6BxPj7Dy5D-Rs",
+      appId: "1:790355774790:android:a0d7d3026b1e4097956947",
+      messagingSenderId: "790355774790",
+      projectId: "my-project-f34c5",
+      storageBucket: "my-project-f34c5.appspot.com",
+    ),
+  );
 
   const AndroidInitializationSettings initializationSettingsAndroid =
       AndroidInitializationSettings('@mipmap/ic_launcher');
@@ -34,27 +37,90 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   final authC = Get.put(LoginController(), permanent: true);
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-        stream: authC.streamAuthStatus,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            print(snapshot);
-            return GetMaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: "Aplikasi LMS SMP Serang Baru",
-              initialRoute: snapshot.data != null &&
-                      snapshot.data!.emailVerified == true
-                  ? Routes.HOME
-                  : Routes.LOGIN,
-              getPages: AppPages.routes,
-              theme: ThemeData(
-                primarySwatch: Colors.indigo,
-              ),
-            );
-          }
+    return FutureBuilder<User?>(
+      future: authC.getCurrentUser(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
           return LoadingView();
-        });
+        }
+
+        if (!snapshot.hasData || snapshot.data == null) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Aplikasi LMS SMP Serang Baru",
+            initialRoute: Routes.LOGIN,
+            getPages: AppPages.routes,
+            theme: ThemeData(
+              primarySwatch: Colors.indigo,
+            ),
+          );
+        } else {
+          return FutureBuilder<DocumentSnapshot>(
+            future: authC.getUserData(snapshot.data!),
+            builder: (context, userSnapshot) {
+              if (userSnapshot.connectionState == ConnectionState.waiting) {
+                return LoadingView();
+              }
+
+              if (userSnapshot.hasError) {
+                return _buildErrorScreen(
+                    'Terjadi kesalahan saat mengambil data pengguna.');
+              }
+
+              if (!userSnapshot.hasData || userSnapshot.data == null) {
+                return _buildErrorScreen('Data pengguna tidak ditemukan.');
+              }
+
+              final userData =
+                  userSnapshot.data!.data() as Map<String, dynamic>?;
+              if (userData == null) {
+                return _buildErrorScreen('Data pengguna tidak valid.');
+              }
+
+              final String role = userData['role'];
+
+              if (role == 'Guru') {
+                return GetMaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: "Aplikasi LMS SMP Serang Baru",
+                  initialRoute: Routes.HOME_GURU,
+                  getPages: AppPages.routes,
+                  theme: ThemeData(
+                    primarySwatch: Colors.indigo,
+                  ),
+                );
+              } else if (role == 'Siswa') {
+                return GetMaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: "Aplikasi LMS SMP Serang Baru",
+                  initialRoute: Routes.HOME,
+                  getPages: AppPages.routes,
+                  theme: ThemeData(
+                    primarySwatch: Colors.indigo,
+                  ),
+                );
+              } else {
+                return _buildErrorScreen('Role pengguna tidak valid.');
+              }
+            },
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildErrorScreen(String message) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Aplikasi LMS SMP Serang Baru",
+      home: Scaffold(
+        body: Center(
+          child: Text(message),
+        ),
+      ),
+    );
   }
 }
