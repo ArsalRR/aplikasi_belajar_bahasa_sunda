b275cb09a639a3f4b2a48e1725de23ac4f214e67
ferikw <ferikusuma41@gmail.com>
2024-06-18 18:21:33 +0700

    Update Backend halaman Loading

--- lib/app/utils/loading.dart
+++ lib/app/utils/loading.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-class LoadingView  extends StatelessWidget {
+class LoadingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(child: CircularProgressIndicator()),
       ),
